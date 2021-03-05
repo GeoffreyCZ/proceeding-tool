c9c3a3dc8c05721162b1e2861913ef460db6c013
Michal Kroupa <mk@2it.cz>
2021-03-05 17:11:27 +0100

    finished first draft

---
+++ src/components/Children.vue
@@ -0,0 +1,18 @@
+<template>
+  <Person title="Nezletilé dítě"></Person>
+</template>
+
+<script>
+import Person from "@/components/Person";
+
+export default {
+  name: "Children",
+  components: {Person},
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
--- src/components/Determination.vue
+++ src/components/Determination.vue
@@ -7,24 +7,72 @@
         outlined
     >
     </v-select>
+    <Children
+        v-for="(child, index) in children"
+        :key="index"
+        :is="child"
+    />
+    <v-btn
+        elevation="2"
+        outlined
+        @click="add"
+    >Přidat dítě
+    </v-btn>
     <Person title="Matka"/>
     <Person title="Otec"/>
-    <div v-if="proposer === 'Jiná osoba'">
-      <Person title="Navrhovatel"/>
+    <Person title="Navrhovatel" v-if="proposer === 'Jiná osoba'"/>
+    <v-textarea
+        outlined
+        name="input-7-4"
+        label="Zdůvodnění"
+    ></v-textarea>
+    <v-select
+        v-model="carer"
+        :items="carerOptions"
+        label="Komu by dítě mělo být svěřeno"
+        outlined
+    ></v-select>
+    <v-text-field
+        v-if="carer === 'Matce'"
+        label="V jaké výši by mělo být výživné stanoveno otci"
+        outlined/>
+    <v-text-field
+        v-else-if="carer === 'Otci'"
+        label="V jaké výši by mělo být výživné stanoveno matce"
+        outlined/>
+    <div v-else-if="carer === 'Jiné osobě'">
+      <v-text-field
+          label="V jaké výši by mělo být výživné stanoveno matce"
+          outlined/>
+      <v-text-field
+          label="V jaké výši by mělo být výživné stanoveno otci"
+          outlined/>
     </div>
   </div>
 </template>
 
 <script>
 import Person from "@/components/Person";
+import Children from "@/components/Children";
 
 export default {
   name: "Determination",
-  components: {Person},
+
+  components: {Children, Person},
+
   data: () => ({
     proposer: '',
-    proposerOptions: ['Matka', 'Otec', 'Jiná osoba']
-  })
+    proposerOptions: ['Matka', 'Otec', 'Jiná osoba'],
+    carer: '',
+    carerOptions: ['Matce', 'Otci', 'Jiné osobě'],
+    children: [Children],
+  }),
+
+  methods: {
+    add() {
+      this.children.push(Children)
+    }
+  }
 }
 </script>
 
